8c0a267a0b31f18b608f7970c5d46d374198e0fa
Rian <rian.sj@gmail.com>
2019-02-16 17:21:54 +0700

    add 4.js js4

---
+++ js4/4.js
@@ -0,0 +1,15 @@
+//Create an express server that sends all the users (that is not you) wrapped in <h3> user </h3> tag when you visit XXXX.garagescript.org/users.
+
+const fs = require("fs");
+const express = require("express")
+const app = express();
+const handler = (req, res) => {
+  fs.readdir('/home', (err, dir) => {
+    if (err) throw err;
+    
+    const users = dir.filter( name => name !== 'rian' ).join('<br/>');
+    res.send(`<h3>${users}</h3>`);
+  });
+};
+app.get('/users', handler);
+app.listen(4450);
